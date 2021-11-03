0706c5b9689790ecd6f3bf6008818d2336cf1cbf
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2021-11-03 22:24:46 +0300

    First React basic app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Card from './Components/card';
+import Collapse from './Components/Collapse';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <div className="row" >
+        <div className="card-group w-100">
+          <div className="col" >
+            <Collapse href="Card1href">
+              <Card cardTitle='Title 1'
+                cardText="Lorem Text 1"
+                updatedTime="Last updated 2 min ago "
+                image="https://picsum.photos/id/100/500/500" />
+            </Collapse>
+          </div>
+
+
+          <div className="col" >
+
+
+            <Collapse href="Card2href">
+              <Card cardTitle='Title 2'
+                cardText="Lorem Text 1"
+                updatedTime="Last updated 2 min ago "
+                image="https://picsum.photos/id/1001/500/500"
+              />
+            </Collapse>
+          </div>
+          <div className="col" >
+
+            <Collapse href="Card3href">
+              <Card cardTitle='Title 3'
+                cardText="Lorem Text 1"
+                updatedTime="Last updated 2 min ago "
+                image="https://picsum.photos/id/1004/500/500" />
+            </Collapse>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
